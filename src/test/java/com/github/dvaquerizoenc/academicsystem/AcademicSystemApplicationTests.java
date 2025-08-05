b015f942ff4bb2fa1c530c19382ba0f53b11f4f6
David Vaquerizo <david@pay-cloud.es>
2025-08-05 18:08:16 +0200

    Agregando proyecto spring boot

---
+++ src/test/java/com/github/dvaquerizoenc/academicsystem/AcademicSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dvaquerizoenc.academicsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
